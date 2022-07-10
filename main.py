61caeb89e1601fe39596d6bf0eab2e9206ad9123
Riwaj Mainali <riwaj227@gmail.com>
2022-07-10 16:13:52 -0500

    basic twitter scraper
    twitter scraper with some customization.

--- main.py
+++ main.py
@@ -2,23 +2,37 @@ import pandas as pd
 import snscrape.modules.twitter as sntwitter
 import itertools
 
-# our search term, using syntax for Twitter's Advanced Search
+ntweets = input("Number of tweets to scrape: ")
 search = input("Enter search term: ")
-date= input("Enter date press nil for all dates: ")
-location= input("Enter location press nil for all location: ")
-if location == "nil":
+location= input("Enter location leave blank for all location: ")
+if location == "":
     location = None
-if date == "nil":
-    date = None
+else:
+    search = search + " near " + "{location}"
+
+datef= input("Enter from date leave blank for all: ")
+datet= input("Enter to date leave blank for all: ")
+
+if datef!="":
+    search = search + " since:" + datef
+elif datet!="":
+    search = search + " until:" + datet
+elif datef != "" and datet != "":
+    search = search + " since: {datef} until: {datet}"
+
+
     
 # the scraped tweets, this is a generator
 scraped_tweets = sntwitter.TwitterSearchScraper(search).get_items()
 
 # slicing the generator to keep only the first 100 tweets
-sliced_scraped_tweets = itertools.islice(scraped_tweets, 100)
+sliced_scraped_tweets = itertools.islice(scraped_tweets, ntweets)
 
 # convert to a DataFrame and keep only relevant columns
-df = pd.DataFrame(sliced_scraped_tweets)[['date', 'content']]
+df = pd.DataFrame(sliced_scraped_tweets)[['date', 'content', 'lang']]
+index_to_drop=df[df['lang'] != 'en'].index
+df.drop(index_to_drop, inplace=True)
+df.drop(['lang'], axis=1, inplace=True)
 print(df.head())
 csv_data= df.to_csv('tweets_df2.csv', index=False)
 
