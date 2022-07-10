d26f99f0c34eb3d6a7f90cf911c065897e499c48
Riwaj Mainali <riwaj227@gmail.com>
2022-07-10 16:32:41 -0500

    cleaning up and adding comments
    fixed an error: needed ntweets to be defined as int

--- main.py
+++ main.py
@@ -2,37 +2,41 @@ import pandas as pd
 import snscrape.modules.twitter as sntwitter
 import itertools
 
-ntweets = input("Number of tweets to scrape: ")
-search = input("Enter search term: ")
-location= input("Enter location leave blank for all location: ")
-if location == "":
-    location = None
-else:
-    search = search + " near " + "{location}"
+# creating search query
+def create_search():
+    ntweets = input("Number of tweets to scrape: ")
+    search = input("Enter search term: ")
+    location= input("Enter location leave blank for all location: ")
+    if location == "":
+        location = None
+    else:
+        search = search + " near " + "{location}"
 
-datef= input("Enter from date leave blank for all: ")
-datet= input("Enter to date leave blank for all: ")
+    datef= input("Enter from date leave blank for all: ")
+    datet= input("Enter to date leave blank for all: ")
 
-if datef!="":
-    search = search + " since:" + datef
-elif datet!="":
-    search = search + " until:" + datet
-elif datef != "" and datet != "":
-    search = search + " since: {datef} until: {datet}"
+    if datef!="":
+        search = search + " since:" + datef
+    elif datet!="":
+        search = search + " until:" + datet
+    elif datef != "" and datet != "":
+        search = search + " since: {datef} until: {datet}"
+    return search, ntweets
 
 
-    
-# the scraped tweets, this is a generator
+search, ntweets = create_search()
+# getting data using snscrape.modules.twitter
 scraped_tweets = sntwitter.TwitterSearchScraper(search).get_items()
 
-# slicing the generator to keep only the first 100 tweets
-sliced_scraped_tweets = itertools.islice(scraped_tweets, ntweets)
+# creating dataframe
+sliced_scraped_tweets = itertools.islice(scraped_tweets,int(ntweets))
 
-# convert to a DataFrame and keep only relevant columns
+#clean up data
 df = pd.DataFrame(sliced_scraped_tweets)[['date', 'content', 'lang']]
 index_to_drop=df[df['lang'] != 'en'].index
 df.drop(index_to_drop, inplace=True)
 df.drop(['lang'], axis=1, inplace=True)
-print(df.head())
+
+#saving data
 csv_data= df.to_csv('tweets_df2.csv', index=False)
 
