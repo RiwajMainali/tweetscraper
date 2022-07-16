8c4954e6fb45ebcd612f710ea3d09de8c646360d
Riwaj Mainali <62513760+RiwajMainali@users.noreply.github.com>
2022-07-15 23:41:03 -0500

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,11 @@
+import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
+
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=600, height=600)
+
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
